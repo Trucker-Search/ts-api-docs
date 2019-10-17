7d96c3f43d7ca5a9c828bb7d2cdc4b53e765d41d
Trucker-Search <jonts@directfreight.com>
2019-10-17 10:13:56 -0500

    synced with df-api-docs

--- scripts/dropbox2repo.sh
+++ scripts/dropbox2repo.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-cd /var/www/dfwdbi/api/ts-api-docs
+export repo=$1 #df-api-docs OR ts-api-docs
+export project=$2 #directfreight OR truckersearch
+cd /var/www/dfwdbi/api/$repo
 pwd
-export APIDOCS_PATH="/home/catalyst/Dropbox/directfreight/apidocs/truckersearch"
+export APIDOCS_PATH="/home/catalyst/Dropbox/directfreight/apidocs/$project"
 if ! diff -q web_deploy/swagger-nocode.yaml $APIDOCS_PATH/swagger20-with-extensions.yaml; then
-        sed -i "s/example: [\"']\(\[.*\]\)[\"']$/example: \1/g" $APIDOCS_PATH/swagger20-with-extensions.yaml #remove extra quotes from examples
+        #sed -i "s/example: [\"']\(\[.*\]\)[\"']$/example: \1/g" $APIDOCS_PATH/swagger20-with-extensions.yaml #remove extra quotes from examples
+        perl -i ./scripts/clean_example.pl $APIDOCS_PATH/swagger20-with-extensions.yaml #remove extra quotes from examples
         cp $APIDOCS_PATH/swagger20-with-extensions.yaml web_deploy/swagger-nocode.yaml
         cp $APIDOCS_PATH/swagger20-with-extensions.yaml restlet_studio/swagger.yaml
         git pull --no-edit #just in case
