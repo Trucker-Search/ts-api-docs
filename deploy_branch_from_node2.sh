e693d87283435fa210979184b51473704213d0ea
Trucker-Search <jonts@directfreight.com>
2022-03-22 08:56:13 -0500

    saving modified files

--- deploy_branch_from_node2.sh
+++ deploy_branch_from_node2.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
+if test "$1" = "swaggerhub"; then
 export TRAVIS_BRANCH='swaggerhub'
+elif test "$1" = "master"; then
+    export TRAVIS_BRANCH='master'
+else
+    echo "invalid branch '$1'"
+    exit
+fi
+git pull
+git commit -a -m "saving modified files"
+git checkout $TRAVIS_BRANCH
+git remote -v
+git remote set-url origin https://github.com/Trucker-Search/ts-api-docs.git
+git remote -v
+
+source ../set_gh_token.df  #set the GH_TOKEN
+
+if test "$1" = "master"; then
+    npm run deploy
+else
 npm run deploy-branch
+fi
+
+./scripts/build.js
